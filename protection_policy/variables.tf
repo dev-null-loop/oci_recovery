d0ed244c286a11bd030f81636daa22ac723b92c1
dev-null-loop <dev.null.loop@gmail.com>
2026-05-18 15:47:09 +0300

    Fix RECOVERY variable descriptions

--- protection_policy/variables.tf
+++ protection_policy/variables.tf
@@ -1,38 +1,44 @@
 variable "backup_retention_period_in_days" {
-  description = "The maximum number of days to retain backups for a protected database."
+  description = "(Required) (Updatable) The maximum number of days to retain backups for a protected database."
   type        = number
 }
 
 variable "compartment_id" {
-  description = "Compartment Identifier"
+  description = "(Required) (Updatable) Compartment Identifier"
   type        = string
 }
 
 variable "defined_tags" {
-  description = "Defined tags for this resource. Each key is predefined and scoped to a namespace. Example: `{\"foo-namespace.bar-key\": \"value\"}`. For more information, see [Resource Tags](https://docs.oracle.com/en-us/iaas/Content/General/Concepts/resourcetags.htm)"
+  description = "(Optional) (Updatable) Defined tags for this resource. Each key is predefined and scoped to a namespace. Example: `{\"foo-namespace.bar-key\": \"value\"}`. For more information, see [Resource Tags](https://docs.oracle.com/en-us/iaas/Content/General/Concepts/resourcetags.htm)"
   type        = map(string)
   default     = null
 }
 
 variable "display_name" {
-  description = "A user provided name for the protection policy. The 'displayName' does not have to be unique, and it can be modified. Avoid entering confidential information."
+  description = "(Required) (Updatable) A user provided name for the protection policy. The 'displayName' does not have to be unique, and it can be modified. Avoid entering confidential information."
   type        = string
 }
 
 variable "freeform_tags" {
-  description = "Simple key-value pair that is applied without any predefined name, type or scope. Exists for cross-compatibility only. Example: `{\"bar-key\": \"value\"}`"
+  description = "(Optional) (Updatable) Simple key-value pair that is applied without any predefined name, type or scope. Exists for cross-compatibility only. Example: `{\"bar-key\": \"value\"}`"
   type        = map(string)
   default     = {}
 }
 
 variable "must_enforce_cloud_locality" {
-  description = "Indicates whether the protection policy enforces Recovery Service to retain backups in the same cloud service environment where your Oracle Database is provisioned. This parameter is applicable if your Oracle Database runs in a different cloud service environment, such as Microsoft Azure. If you set the mustEnforceCloudLocality parameter to TRUE, then Recovery Service stores the database backups locally in the same cloud service environment where the database resides. For example, if your Oracle Database is provisioned on Microsoft Azure, then Recovery Service stores the database backups in Azure. Note: You cannot change the mustEnforceCloudLocality setting for a protection policy after you create it."
+  description = "(Optional) Indicates whether the protection policy enforces Recovery Service to retain backups in the same cloud service environment where your Oracle Database is provisioned. This parameter is applicable if your Oracle Database runs in a different cloud service environment, such as Microsoft Azure. If you set the mustEnforceCloudLocality parameter to TRUE, then Recovery Service stores the database backups locally in the same cloud service environment where the database resides. For example, if your Oracle Database is provisioned on Microsoft Azure, then Recovery Service stores the database backups in Azure. Note: You cannot change the mustEnforceCloudLocality setting for a protection policy after you create it."
   type        = bool
   default     = null
 }
 
 variable "policy_locked_date_time" {
-  description = "An RFC3339 formatted datetime string that specifies the exact date and time for the retention lock to take effect and permanently lock the retention period defined in the policy.\n* The retention lock feature controls whether Recovery Service strictly preserves backups for the duration defined in a policy. Retention lock is useful to enforce recovery window compliance and to prevent unintentional modifications to protected database backups.\n* Recovery Service enforces a 14-day delay before the retention lock set for a policy can take effect. Therefore, you must set policyLockedDateTime  to a date that occurs 14 days after the current date.\n* For example, assuming that the current date is Aug 1, 2023 9 pm, you can set policyLockedDateTime  to '2023-08-15T21:00:00.600Z' (Aug 15, 2023, 9:00 pm), or greater.\n* During the 14-day delay period, you can either increase or decrease the retention period in the policy.\n* However, you are only allowed to increase the retention period on or after the retention lock date.\n* You cannot change the value of policyLockedDateTime if the retention lock is already in effect."
+  description = "(Optional) (Updatable) An RFC3339 formatted datetime string that specifies the exact date and time for the retention lock to take effect and permanently lock the retention period defined in the policy.
+* The retention lock feature controls whether Recovery Service strictly preserves backups for the duration defined in a policy. Retention lock is useful to enforce recovery window compliance and to prevent unintentional modifications to protected database backups.
+* Recovery Service enforces a 14-day delay before the retention lock set for a policy can take effect. Therefore, you must set policyLockedDateTime  to a date that occurs 14 days after the current date.
+* For example, assuming that the current date is Aug 1, 2023 9 pm, you can set policyLockedDateTime  to '2023-08-15T21:00:00.600Z' (Aug 15, 2023, 9:00 pm), or greater.
+* During the 14-day delay period, you can either increase or decrease the retention period in the policy.
+* However, you are only allowed to increase the retention period on or after the retention lock date.
+* You cannot change the value of policyLockedDateTime if the retention lock is already in effect."
   type        = string
   default     = null
 }
--- recovery_service_subnet/variables.tf
+++ recovery_service_subnet/variables.tf
@@ -1,44 +1,44 @@
 variable "compartment_id" {
-  description = "The compartment OCID."
+  description = "(Required) (Updatable) The compartment OCID."
   type        = string
 }
 
 variable "defined_tags" {
-  description = "Defined tags for this resource. Each key is predefined and scoped to a namespace. Example: `{\"foo-namespace.bar-key\": \"value\"}`. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm)"
+  description = "(Optional) (Updatable) Defined tags for this resource. Each key is predefined and scoped to a namespace. Example: `{\"foo-namespace.bar-key\": \"value\"}`. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm)"
   type        = map(string)
   default     = null
 }
 
 variable "display_name" {
-  description = "A user-provided name for the recovery service subnet. The 'displayName' does not have to be unique, and it can be modified. Avoid entering confidential information."
+  description = "(Required) (Updatable) A user-provided name for the recovery service subnet. The 'displayName' does not have to be unique, and it can be modified. Avoid entering confidential information."
   type        = string
 }
 
 variable "freeform_tags" {
-  description = "Simple key-value pair that is applied without any predefined name, type or scope. Exists for cross-compatibility only. Example: `{\"bar-key\": \"value\"}`"
+  description = "(Optional) (Updatable) Simple key-value pair that is applied without any predefined name, type or scope. Exists for cross-compatibility only. Example: `{\"bar-key\": \"value\"}`"
   type        = map(string)
   default     = {}
 }
 
 variable "nsg_ids" {
-  description = "A list of network security group (NSG) OCIDs that are associated with the Recovery Service subnet. You can specify a maximum of 5 unique OCIDs, which implies that you can associate a maximum of 5 NSGs to each Recovery Service subnet. Specify an empty array if you want to remove all the associated NSGs from a Recovery Service subnet. See [Network Security Groups](https://docs.cloud.oracle.com/iaas/api/#/en/iaas/latest/NetworkSecurityGroup/) for more information."
+  description = "(Optional) (Updatable) A list of network security group (NSG) OCIDs that are associated with the Recovery Service subnet. You can specify a maximum of 5 unique OCIDs, which implies that you can associate a maximum of 5 NSGs to each Recovery Service subnet. Specify an empty array if you want to remove all the associated NSGs from a Recovery Service subnet. See [Network Security Groups](https://docs.cloud.oracle.com/iaas/api/#/en/iaas/latest/NetworkSecurityGroup/) for more information."
   type        = list(string)
   default     = []
 }
 
 variable "subnet_id" {
-  description = "Deprecated. One of the subnets associated with the Recovery Service subnet."
+  description = "(Optional) Deprecated. One of the subnets associated with the Recovery Service subnet."
   type        = string
   default     = null
 }
 
 variable "subnets" {
-  description = "A list of OCIDs of the subnets associated with the Recovery Service subnet."
+  description = "(Optional) (Updatable) A list of OCIDs of the subnets associated with the Recovery Service subnet."
   type        = list(string)
   default     = []
 }
 
 variable "vcn_id" {
-  description = "The OCID of the virtual cloud network (VCN) that contains the recovery service subnet. You can create a single recovery service subnet per VCN."
+  description = "(Required) The OCID of the virtual cloud network (VCN) that contains the recovery service subnet. You can create a single recovery service subnet per VCN."
   type        = string
 }
